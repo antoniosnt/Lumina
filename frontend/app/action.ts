dc4d24180773c5781000e85adc6597331c34cff8
antoniosnt <antoniosnt.dev@gmail.com>
2026-03-18 09:20:29 -0300

    style: adjust css reposivity and style form

--- frontend/app/action.ts
+++ frontend/app/action.ts
@@ -4,24 +4,24 @@ import { cookies } from "next/headers";
 import { axiosInstance } from "@/lib/utils";
 
 const onSubmit = async (formData: FormData) => {
-  try {
-    const payload = {
-      username: formData.get("email") as string,
-      password: formData.get("password") as string,
-    };
+	try {
+		const payload = {
+			username: formData.get("email") as string,
+			password: formData.get("password") as string,
+		};
 
-    const response = await axiosInstance("/api/auth/login/").post(payload);
-    const { access, refresh } = response.data as {
-      access: string;
-      refresh: string;
-    };
+		const response = await axiosInstance("/api/auth/login/").post(payload);
+		const { access, refresh } = response.data as {
+			access: string;
+			refresh: string;
+		};
 
-    const cookieStore = await cookies();
-    cookieStore.set("access_token", access, { httpOnly: true, secure: true });
-    cookieStore.set("refresh_token", refresh, { httpOnly: true, secure: true });
-  } catch (e) {
-    console.error(e);
-  }
+		const cookieStore = await cookies();
+		cookieStore.set("access_token", access, { httpOnly: true, secure: true });
+		cookieStore.set("refresh_token", refresh, { httpOnly: true, secure: true });
+	} catch (e) {
+		console.error(e);
+	}
 };
 
 export default onSubmit;
