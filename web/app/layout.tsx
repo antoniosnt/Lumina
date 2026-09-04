75fb5a631d7681f24ef0508a12302e8e7023f073
antoniosnt <antoniosnt.dev@gmail.com>
2026-09-04 10:04:45 -0300

    style: dark theme

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark" style={{ colorScheme: "dark" }}>
       <body className={poppins.className}>{children}</body>
     </html>
   );
